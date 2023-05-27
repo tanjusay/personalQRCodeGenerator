d014d28354d113be8260df6082964a315ec67811
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-05-27 12:58:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 import qrcode
 
+import matplotlib.pyplot as plt
+
+from io import BytesIO
+
 def generate_qr_code(data):
 
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -14,13 +18,29 @@ def generate_qr_code(data):
 
     return qr_img
 
+def plot_qr_code(qr_img):
+
+    # Convert PIL image to NumPy array
+
+    qr_array = plt.imshow(qr_img)
+
+    plt.axis('off')  # Hide axes
+
+    buf = BytesIO()
+
+    plt.savefig(buf, format='png')
+
+    buf.seek(0)
+
+    return buf
+
 def main():
 
     st.title("QR Code Generator")
 
     st.write("Enter text or URL to generate a QR code")
 
-
+    # User input
 
     input_data = st.text_input("Enter text or URL")
 
@@ -28,15 +48,19 @@ def main():
 
         if input_data:
 
-
+            # Generate QR code
 
             qr_code = generate_qr_code(input_data)
 
+            # Display the QR code image
 
+            qr_buffer = plot_qr_code(qr_code)
 
-            st.image(qr_code)
+            st.image(qr_buffer, use_column_width=True)
 
 if __name__ == '__main__':
 
     main()
+
+
 
